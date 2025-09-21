1c6e2dd9a0a05ef6bd60083cb64bb2e2b53683c0
oliveirahilde95-coder <oliveirahilde95@gmail.com>
2025-09-21 17:47:38 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,75 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+
+struct Carta {
+    char estado;
+    char codigo[4]; 
+    char cidade[50];
+    int populacao;
+    float area;
+    float PIB;
+    int pontosTuristicos;
+};
+
+
+void lerDadosCarta(struct Carta *carta) {
+    printf("--- Digite os dados da carta ---\n");
+    
+    
+    printf("Estado (A-H): ");
+    scanf(" %c", &carta->estado); 
+
+    
+    printf("Codigo da Carta (ex: A01): ");
+    scanf("%s", carta->codigo);
+
+    
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", carta->cidade); 
+
+    
+    printf("Populacao: ");
+    scanf("%d", &carta->populacao);
+
+    printf("Area (em km²): ");
+    scanf("%f", &carta->area);
+
+    printf("PIB (em bilhoes de reais): ");
+    scanf("%f", &carta->PIB);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &carta->pontosTuristicos);
+}
+
+
+void exibirDadosCarta(struct Carta carta) {
+    printf("--- Informacoes da Carta ---\n");
+    printf("Estado: %c\n", carta.estado);
+    printf("Codigo da Carta: %s\n", carta.codigo);
+    printf("Nome da Cidade: %s\n", carta.cidade);
+    printf("Populacao: %d\n", carta.populacao);
+    printf("Area: %.2f km²\n", carta.area);
+    printf("PIB: %.2f bilhoes de reais\n", carta.PIB);
+    printf("Numero de Pontos Turisticos: %d\n", carta.pontosTuristicos);
+}
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    struct Carta carta1, carta2;
 
-  // Área para entrada de dados
+    printf("--- Cadastro da Carta 1 ---\n");
+    lerDadosCarta(&carta1);
 
-  // Área para exibição dos dados da cidade
+    printf("\n--- Cadastro da Carta 2 ---\n");
+    lerDadosCarta(&carta2);
+    
+    printf("\n==================================\n");
+    printf("--- Cartas Cadastradas ---\n");
+    
+    printf("\n--- Carta 1 ---\n");
+    exibirDadosCarta(carta1);
 
-return 0;
-} 
+    printf("\n--- Carta 2 ---\n");
+    exibirDadosCarta(carta2);
+
+    return 0;
+}
